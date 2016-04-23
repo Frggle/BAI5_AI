a2210ac84bcf69ec219f055a999054e57c2b5ed6
marc <marc.kaepke@gmail.com>
2016-04-23 13:57:11 +0200

    change database from sqlite3 to postgres/psql -> nessecary for heruko

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160422111940) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "plans", force: :cascade do |t|
     t.string   "content"
     t.datetime "created_at", null: false
